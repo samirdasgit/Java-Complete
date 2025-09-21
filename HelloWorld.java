0d0ff59c28d77727abb2d835672f9c2b527917ae
Samir Das <samir.das1055@gmail.com>
2025-09-21 08:10:00 +0530

    Update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-//01. Write a program to print "Hello, World!"
+/* 01. Write a program to print "Hello, World!" */
 public class HelloWorld {
   public static void main(String args[]) {
     System.out.println("This is a Hello World program in Java");
